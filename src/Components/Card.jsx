26ae8cb3d7d4c2304f1b32f62bf887c812ee8b4b
Kazi Tasin Hossen <kaziweblab@gmail.com>
2026-03-22 17:25:29 +0600

    fix: updated card component and spelling issues

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,13 +3,25 @@ import React, { useState } from "react";
 import "./card.css";
 
 export default function Card(props) {
+  const truncateString = (str, num) => {
+    if (str.length > num) {
+      return str.slice(0, num) + "...";
+    } else {
+      return str;
+    }
+  };
 
   return (
-    <section className={`card ${props.class}`} /*onClick={handelClick}*/>
+    <section className={`card ${props.class}`}>
       <img src={props.image} alt="product Image" className="card-image" />
-      <h2 className="card-title">{props.title}</h2>
+      {props.title && (
+        <h2 className="card-title">{truncateString(props.title, 30)}</h2>
+      )}
       <p className="card-price">Prize : {props.price}</p>
-      <p className="card-desc">Description : {props.desc}</p>
+      <p className="card-desc">
+        Description :{" "}
+        {props.desc && <span>{truncateString(props.desc, 50)}</span>}
+      </p>
       {props.btn}
     </section>
   );
